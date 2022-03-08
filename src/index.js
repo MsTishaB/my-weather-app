52ff859a9bf7f11debe79f0743d1117bc60458de
MsTishaB <98257816+MsTishaB@users.noreply.github.com>
2022-03-08 22:16:35 +0000

    Added humidity, feels like temp and wind speed
    wind speed and feels like temperature change when you click the units radio buttons

--- src/index.js
+++ src/index.js
@@ -15,25 +15,26 @@ function currentTimeInfo(timestamp) {
 
 function showCityTemperature(response) {
 	celsiusTemp = Math.round(response.data.main.temp);
-	let localTime = currentTimeInfo(
-		response.data.dt * 1000 + response.data.timezone * 1000
-	);
+	let time = response.data.dt * 1000;
+	let timezone = response.data.timezone * 1000;
+	let localTime = currentTimeInfo(time + timezone);
+	windSpeed = Math.round((response.data.wind.speed * 3600) / 1000);
+	let conditions = response.data.weather[0].description;
+	let icon = response.data.weather[0].icon;
+	let humidity = response.data.main.humidity;
+	celsiusFeelsTemp = Math.round(response.data.main.feels_like);
 	document.querySelector("#current-temp").innerHTML = `${celsiusTemp}°C`;
 	document.querySelector("#current-city").innerHTML = response.data.name;
-	document.querySelector("#wind-speed").innerHTML = `Winds ${Math.round(
-		response.data.wind.speed
-	)}`;
-	document.querySelector(
-		"#description"
-	).innerHTML = `${response.data.weather[0].description}`;
-
+	document.querySelector("#wind-speed").innerHTML = `Winds ${windSpeed}kmph`;
+	document.querySelector("#description").innerHTML = `${conditions}`;
 	document
 		.querySelector("#icon")
-		.setAttribute(
-			"src",
-			`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-		);
+		.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
 	document.querySelector("#date").innerHTML = `Local Time: ${localTime}`;
+	document.querySelector("#humidity").innerHTML = `Humidity ${humidity}%`;
+	document.querySelector(
+		"#feels-like"
+	).innerHTML = `Feels Like ${celsiusFeelsTemp}°C`;
 	console.log(response.data);
 }
 
@@ -79,18 +80,32 @@ currentLocationCheckBox.addEventListener("change", findGeoLocation);
 
 function changeToCelsius() {
 	let showcelsiusTemp = document.querySelector("#current-temp");
+	let showCelsiusFeelsTemp = document.querySelector("#feels-like");
+	let showWindSpeedMetric = document.querySelector("#wind-speed");
 	showcelsiusTemp.innerHTML = `${celsiusTemp}°C`;
+	showCelsiusFeelsTemp.innerHTML = `Feels like ${celsiusFeelsTemp}°C`;
+	showWindSpeedMetric.innerHTML = `Winds ${windSpeed}kmph`;
+	console.log(showWindSpeedMetric);
 }
 
 function changetoFahrenheit() {
 	let fahrenheitTemp = document.querySelector("#current-temp");
+	let fahrenheitFeelsTemp = document.querySelector("#feels-like");
+	let windSpeedImperial = document.querySelector("#wind-speed");
 	fahrenheitTemp.innerHTML = `${Math.round((celsiusTemp * 9) / 5 + 32)}°F`;
+	fahrenheitFeelsTemp.innerHTML = `Feels like ${Math.round(
+		(celsiusFeelsTemp * 9) / 5 + 32
+	)}°F`;
+	windSpeedImperial.innerHTML = `Winds ${Math.round(windSpeed * 0.621371)}mph`;
+	console.log(windSpeedImperial);
 }
 
 let celsiusRadioButton = document.querySelector("#flexRadioDefault1");
 celsiusRadioButton.addEventListener("click", changeToCelsius);
 
 let celsiusTemp = null;
+let celsiusFeelsTemp = null;
+let windSpeed = null;
 
 let fahrenheitRadioButton = document.querySelector("#flexRadioDefault2");
 fahrenheitRadioButton.addEventListener("click", changetoFahrenheit);
